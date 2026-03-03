9827e7e4edc707570b82c548883d556796c0cab0
proyanfromyt <proyanfromyt@gmail.com>
2026-03-03 11:32:06 +0800

    NEW UPDATE REGISTRATION REPORT USER.CSHTML

---
+++ Migrations/20260302143843_AddProgramToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MonitoringSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProgramToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Program",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Program",
+                table: "AspNetUsers");
+        }
+    }
+}
